b854f5e154c08e58867c8fd58bf91c77195d3c04
Sam Litowitz <samlitowitz@users.noreply.github.com>
2022-05-22 11:44:10 -0400

    Config argument to option

--- src/App/Console/CLI.php
+++ src/App/Console/CLI.php
@@ -5,18 +5,20 @@ namespace PhpHephaestus\App\Console;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
 
 final class CLI extends Application
 {
-	public const CONFIG_ARG = 'config';
+	public const CONFIG_OPT = 'config';
 
 	protected function getDefaultInputDefinition(): InputDefinition
 	{
 		$definition = parent::getDefaultInputDefinition();
-		$definition->addArgument(
-			new InputArgument(
-				self::CONFIG_ARG,
-				InputArgument::REQUIRED,
+		$definition->addOption(
+			new InputOption(
+				self::CONFIG_OPT,
+				null,
+				InputOption::VALUE_REQUIRED,
 				'Configuration file to use',
 				'php-hephaestus.json'
 			)
